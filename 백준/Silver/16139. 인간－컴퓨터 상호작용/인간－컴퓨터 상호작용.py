866ec4d3c6397a1705cce93a4a980655256b3be4
won_seop <123632194+won-seoop@users.noreply.github.com>
2026-02-01 09:57:36 +0900

    [Silver I] Title: 인간-컴퓨터 상호작용, Time: 132 ms, Memory: 33432 KB, Score: 50 point -BaekjoonHub

---
+++ 백준/Silver/16139. 인간－컴퓨터 상호작용/인간－컴퓨터 상호작용.py
@@ -0,0 +1,22 @@
+import sys
+
+d=[]
+total = [0] * 26
+
+str_=input()
+q=int(input())
+
+for c in str_:
+    total[ord(c)-97]+=1
+    d.append(total.copy())
+    
+for i in range(q):
+    alpa, l, r = map(str, input().split())
+    l=int(l)
+    r=int(r)
+    
+    if(l==0):
+        print(d[r][ord(alpa)-97])
+    else:
+        print(d[r][ord(alpa)-97] - d[l-1][ord(alpa)-97])
+        
