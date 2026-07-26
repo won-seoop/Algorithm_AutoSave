e840ee41c5e5b061e5dd5ed862d1c686b8b3d9fa
halo <dongjin778@naver.com>
2026-07-26 09:48:56 +0900

    분기별 폴더 구조 변경

---
+++ ETC/COMPANY/HD/Dijkstra/base.java
@@ -0,0 +1,17 @@
+import java.io.*;
+import java.util.*;
+
+class Main{
+public static void main(String[] args) throws IOException{
+    
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    int a = Integer.parseInt(br.readLine());
+
+    System.out.println(a);
+
+
+    
+
+    }
+}
